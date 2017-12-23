18a3be65c5f663b7916d16429ee2cd949f827cf6
Gus <jguszr@gmail.com>
2017-12-23 18:32:09 -0200

    fixing the regex
    fixing the regex not there yet

--- pdfExample.py
+++ pdfExample.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+experimental script to read a pdf file and
+extract credit card information
 
-This is a temporary script file.
 """
 
 base_path = "/users/jguszr/AnacondaProjects/gusOnfinance/db/"
@@ -14,8 +14,28 @@ password = ""
 extracted_text = ""
 
 import PyPDF2
+import re
 file = open(my_file, 'rb')
 
  fileReader = PyPDF2.PdfFileReader(file)
  
 print( fileReader.numPages)
+page = fileReader.getPage(1)
+lines = page.extractText().splitlines()
+len(lines)
+for l in lines:
+    print(l)
+
+txt = page.extractText()
+
+#getting started on the rgex
+re.findall("[(\d\d)\.(\d\d)].", txt)
+re.findall("\d\d\.\d\d[(\w)]*", txt)
+re.findall("\d\d\.\d\d[(\w) \b [(\w)|(\d)]]*[(US$)|(BR$)]", txt)
+
+#better and it is very good first clean up
+for l in  re.split("\d{2}\.\d{2}", txt):
+    print(re.split("\$", l))
+
+
+
